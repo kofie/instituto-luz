61a9d69886c8fcac5d7c2ec6c15054afa310e811
naerthon <naerthon@gmail.com>
2016-04-29 12:53:45 -0300

    Update admin.py

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from .models import Beneficiado,Presenca,Frequencia
-# Register your models here.
-
-admin.site.register(Beneficiado)
-admin.site.register(Presenca)
-admin.site.register(Frequencia)
