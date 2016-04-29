aa5199e9ee08307ef1c0d05a3c2b79b38080fc3c
naerthon guedelho <naerthon@gmail.com>
2016-04-29 12:45:27 -0300

    Initial commit

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Beneficiado,Presenca,Frequencia
 # Register your models here.
+
+admin.site.register(Beneficiado)
+admin.site.register(Presenca)
+admin.site.register(Frequencia)
